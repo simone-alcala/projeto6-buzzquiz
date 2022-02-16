7f734255f38e5f4002da10271846bf297b8f051f
simone-alcala <simone.alcala@gmail.com>
2022-02-15 21:25:52 -0300

    Layout inicial

---
+++ script/script.js
@@ -0,0 +1,27 @@
+
+function loadQuizzes(){
+  let promise = axios.get("https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes");
+  
+  promise.then(renderQuizzes);
+  promise.catch( erro => {
+
+    console.error(erro.response);
+    alert("Não carregou - DEPOIS VOU TIRAR A MENSAGEM");
+  });
+
+}
+
+function renderQuizzes (promise){
+  let quizzes = promise.data;
+  let allQuizzes = document.querySelector(".allQuizzes");
+  allQuizzes.innerHTML="";
+  quizzes.forEach(quiz => {
+    allQuizzes.innerHTML +=
+      `<div class="quiz">
+          <img src="${quiz.image}" alt="" />
+          <a href="#" >${quiz.title}</a>
+      </div> `
+  });
+}
+
+loadQuizzes();
