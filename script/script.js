4ff6472894cb794d975783c98b98c8345b21f403
simone-alcala <simone.alcala@gmail.com>
2022-02-16 00:43:41 -0300

    Tela de responder quiz

--- script/script.js
+++ script/script.js
@@ -19,15 +19,17 @@ function setArrayQuizzes(promise){
 
 function renderQuizzes (quizzes){
   let allQuizzes = document.querySelector(".allQuizzes");
+  
   allQuizzes.innerHTML="";
+
   quizzes.forEach(quiz => {
     let quizImage = gradientImageQuiz(quiz.image);
+    
     allQuizzes.innerHTML +=
-      `<div class="quiz" style="${quizImage}"> <div onclick="openQuiz(quiz)">${quiz.title}</div> </div> `
+      `<div class="quiz" style="${quizImage}"> <div onclick="openQuiz(${quiz.id})">${quiz.title}</div> </div> `;
   });
 }
 
-<<<<<<< HEAD
 loadQuizzes();
 
 //-- CRIAR QUIZZ --//
@@ -51,12 +53,47 @@ function showMain(){
   document.querySelector("main").classList.remove("hidden");
 }
 
-function openQuiz(quiz){
+function openQuiz(quizId){
   closeMain();
+  let arrayQuiz = arrayQuizzes.filter(quiz => quiz.id === quizId );
+  renderQuiz (arrayQuiz);
+}
+
+function renderQuiz(quiz){
+  let answeringQuiz = document.querySelector(".answeringQuiz");
+
+  let gradientTitle = gradientTitleQuiz (quiz[0].image);
+
+  let answeringQuizHTML = 
+    ` <div class="header" style="${gradientTitle}">${quiz[0].title}</div> `;
+    
+    quiz[0].questions.forEach(question => {
+      
+      answeringQuizHTML += `
+        <div class="question"> 
+          <div class="title"> ${question.title}</div>
+          <input class="color hidden" value="${question.color}" /> `;
+
+        question.answers.forEach(answer => {
+          answeringQuizHTML += `
+            <div class="images">
+              <img src="${answer.image}" alt="${answer.image}" />
+              <strong>${answer.text}</strong>
+            </div>
+            <input class="answer hidden" value="${answer.isCorrectAnswer}" /> `;
+        });
+      });
+
+      answeringQuizHTML += ` </div> `;
+  
+  answeringQuiz.innerHTML = answeringQuizHTML;
+  }
+
+function gradientTitleQuiz(quizImage){
+  return `background-image: linear-gradient(0deg, rgba(0, 0, 0, 0.6), 
+                            rgba(0, 0, 0, 0.6)), url(	${quizImage});`;
 }
 
 loadQuizzes();
 
-
-loadQuizzes();
 
